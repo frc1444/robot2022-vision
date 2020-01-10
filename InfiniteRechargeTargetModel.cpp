cd382832b6c6115ba3109929481bdc7a95fe1673
Aaron Hall <catacon@gmail.com>
2020-01-09 21:55:11 -0600

    Added 2020 target model

--- InfiniteRechargeTargetModel.cpp
+++ InfiniteRechargeTargetModel.cpp
@@ -5,15 +5,14 @@ using namespace Lightning;
 InfiniteRechargeTargetModel::InfiniteRechargeTargetModel() : TargetModel()
 {
     _keyPoints = std::vector<cv::Point3d> {
-        {185.76, 73.985, 0},
-        {34.978, 0, 0},
-        {336.542, 0, 0},
-        {84.16, 12.719, 0},
-        {287.36, 12.719, 0},
-        {0, 135.25, 0},
-        {371.52, 135.25, 0},
-        {49.182, 147.97, 0},
-        {322.338, 147.97, 0}
+        {0, 0, 0},
+        {58.6486, 0, 0},
+        {938.3014, 0, 0},
+        {996.95, 0, 0},
+        {278.384, 381, 0},
+        {718.566, 381, 0},
+        {249.047, 431.8, 0},
+        {747.903, 431.8, 0}  
     };
 
     _targetAxes = std::vector<cv::Point3d> {
@@ -33,19 +32,10 @@ std::vector<cv::Point3d> InfiniteRechargeTargetModel::GetSubTargetKeyPoints(int 
     if (subTarget == 0)
     {
         keyPoints = std::vector<cv::Point3d> {
-            {34.978, 0, 0},
-            {84.16, 12.719, 0},
-            {0, 135.25, 0},
-            {49.182, 147.97, 0}
-        };
-    }
-    else
-    {
-        keyPoints = std::vector<cv::Point3d> {
-            {336.542, 0, 0},
-            {287.36, 12.719, 0},
-            {371.52, 135.25, 0},
-            {322.338, 147.97, 0}
+            {0, 0, 0},
+            {996.95, 0, 0},
+            {249.047, 431.8, 0},
+            {747.903, 431.8, 0} 
         };
     }
 
