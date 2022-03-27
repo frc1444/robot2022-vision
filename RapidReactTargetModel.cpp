83e93c6ff9487fca227ab89bbacba4558a432f65
Aaron Hall <aaroncharleshall@gmail.com>
2022-03-26 22:00:34 -0500

    Updated target model for 2022

--- RapidReactTargetModel.cpp
+++ RapidReactTargetModel.cpp
@@ -5,15 +5,10 @@ using namespace Lightning;
 RapidReactTargetModel::RapidReactTargetModel() : TargetModel()
 {
     _keyPoints = std::vector<cv::Point3d> {
-        {498.475, 0, 0},
         {0, 0, 0},
-        {58.6486, 0, 0},
-        {938.3014, 0, 0},
-        {996.95, 0, 0},
-        {278.384, 381, 0},
-        {718.566, 381, 0},
-        {249.047, 431.8, 0},
-        {747.903, 431.8, 0}  
+        {127, 0, 0},
+        {0, 50.8, 0},
+        {127, 50.8, 0},
     };
 
     _targetAxes = std::vector<cv::Point3d> {
@@ -26,22 +21,7 @@ RapidReactTargetModel::RapidReactTargetModel() : TargetModel()
 
 std::vector<cv::Point3d> RapidReactTargetModel::GetSubTargetKeyPoints(int subTarget) const 
 {   
-    // TODO this could be better - may require redesign of target model
-
-    std::vector<cv::Point3d> keyPoints;
-
-    if (subTarget == 0)
-    {
-        // For 2020 this returns the boundingbox corners of the target + center
-        keyPoints = std::vector<cv::Point3d> {
-           // {0, 0, 0},
-            {-498.475, 0, 0},
-            {498.475, 0, 0},
-            {-249.428, 431.8, 0},
-            {249.428, 431.8, 0} 
-        };
-    }
-
-    return keyPoints;
+    // For 2022, there isn't really a sub target, so just return the full target keypoints
+    return _keyPoints;
     
 }
